afde64afbb861243daa523747090528acc5ecda1
danielemaraschin <danikmaraschin@gmail.com>
2021-12-19 06:53:50 -0300

    Learning about descriptor

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -8,7 +8,7 @@ export function logarTempoDeExecucao(){
         const t1 = performance.now();
         const retorno = metodoOriginal();
         const t2 = performance.now();
-        
+        console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000 } segundos`)
         
         return descriptor;
     }
