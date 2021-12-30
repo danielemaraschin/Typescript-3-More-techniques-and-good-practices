622bf4cf95811719d10981f90207614b68ad8ca5
danielemaraschin <danikmaraschin@gmail.com>
2021-12-30 13:00:32 -0300

    resolving the issue with the original 'this'

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -7,7 +7,7 @@ export function logarTempoDeExecucao(){
         const metodoOriginal = descriptor.value; //descriptor é o método original decorado
         descriptor.value = function(){
             const t1 = performance.now();
-            const retorno = metodoOriginal.apply();
+            const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
             console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000 } segundos`) //divide p/1000 para obter valor em segundos
                         //nome do método
