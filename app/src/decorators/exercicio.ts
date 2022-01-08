44c7c0e18d281f1e3c0122dfa4b89f4d365c804e
danielemaraschin <danikmaraschin@gmail.com>
2022-01-07 21:29:54 -0300

    exercise about decorators

--- app/src/decorators/exercicio.ts
+++ app/src/decorators/exercicio.ts
@@ -1,1 +1,7 @@
-/**/
+/*
+Rafaela deseja criar um decorator que,
+ ao ser aplicado em um método, 
+ exibe no console a data na qual o método foi chamado. 
+ Contudo, se o decorator receber como parâmetro dd/MM/yyyy, deve exibir a data neste formato.
+*/
+
