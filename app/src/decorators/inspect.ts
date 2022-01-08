14a0331dd7b4ab8e42df017a3e723fc2d6c454d9
danielemaraschin <danikmaraschin@gmail.com>
2022-01-08 12:53:50 -0300

    working with decorator Descriptor

--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -1,11 +1,11 @@
-export function inspect(){
-    return function(
+export function inspect() {
+    return function (
         target: any, //pode ser constructor(static method) ou prototype (metodo de instancia);
         propertyKey: string, //nome do method
         descriptor: PropertyDescriptor //referencia ao method
-        ){
-        
-    }}
-  
-  
+    ) {
+        const metodoOriginal = descriptor.value;
+    }
+
+
 }
